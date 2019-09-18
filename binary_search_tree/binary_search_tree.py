7f5fce8c4cc9a0e8fe1b94173f59d4367bbee51f
meginks <meganjones.dev@gmail.com>
2019-09-18 15:40:38 -0500

    finished binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+from queue import Queue
+from stack import Stack
 
 class BinarySearchTree:
   def __init__(self, value):
@@ -10,13 +10,38 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value: 
+      if not self.left: 
+        self.left = BinarySearchTree(value)
+      else: 
+        self.left.insert(value)
+    else: 
+      if not self.right: 
+        self.right = BinarySearchTree(value)
+      else: 
+        self.right.insert(value)
 
   def contains(self, target):
-    pass
+    if self.value == target: 
+      return True
+    elif target < self.value: 
+      if self.left: 
+        return self.left.contains(target)
+    else: 
+      if self.right: 
+        return self.right.contains(target)
+    return False
 
   def get_max(self):
-    pass
+    if not self.right: 
+      return self.value  # base case for the get_max function because the left will always have the lower number in a binary tree. right will always be higher
+    else: 
+      return self.right.get_max() 
+   
 
   def for_each(self, cb):
-    pass
+    cb(self.value) 
+    if self.left: 
+      self.left.for_each(cb)
+    if self.right: 
+      self.right.for_each(cb)
